6545d9c36c01826ce3af28b34477ab9043cc11d5
Unsa Memon <unsamemon121@gmail.com>
2025-06-11 19:48:38 +0500

    added error and email utilities for quiz app

---
+++ utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // Create reusable transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD
+    }
+  });
+
+  // Define the email options
+  const mailOptions = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  // Send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
